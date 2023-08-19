02f3b6c4aadd44efecdbff7490aef5c23b8cd4b8
L-LYR <lisicen97@gmail.com>
2023-08-19 04:25:17 +0000

    build: add scripts to download missing jars, add resolver

---
+++ download_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://repo1.maven.org/maven2/com/typesafe/scala-logging/scala-logging-api_2.11/2.1.2/scala-logging-api_2.11-2.1.2.jar
+wget https://repo1.maven.org/maven2/com/typesafe/scala-logging/scala-logging-slf4j_2.11/2.1.2/scala-logging-slf4j_2.11-2.1.2.jar
